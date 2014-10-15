f253fbdbc6078a2539bff4825f96fb37c2ce9f70
Nicholas Westlake <nicholasredlin@gmail.com>
2014-10-15 13:04:50 -0500

    clean deps

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-var ObservStruct = require('observ-struct')
 var Computed = require('observ/computed')
-
 var patcher = require('patcher')
 
 module.exports = DiffData
 
 function DiffData (first, last) {
-  return ObservStruct({
+  return {
     first: first,
     last: last,
     patch: Computed([first, last], function (b, p) {
       return patcher.computePatch(b, p)
-    })
+    }
   })
 }
